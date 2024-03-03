c3eb28c4e8d5888d33822b40a1856c1853d12159
Musaib Iqbal <iqbal.musaib55@gmail.com>
2024-03-03 22:27:14 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
+const dirname = path.resolve();
 
 /** ------------------ IMPORTING CONTROLLERS ------------------ **/
 const homeController = require('../controllers/home_controller');
 const fileController = require('../controllers/file_controller');
-const upload = multer({ dest: 'uploads/files'})
+const upload = multer({ dest: path.join(dirname, 'uploads') })
 
 /** ------------------ MAKING ROUTES ------------------ **/
 router.get('/', homeController.home);
